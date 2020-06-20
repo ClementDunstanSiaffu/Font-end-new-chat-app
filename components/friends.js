4521518621d3b21558cd5f18926b728b8b2778d9
ClementDustanSiaffu <clementsiaffu95@gmail.com>
2020-06-20 13:14:49 +0300

    first commit

---
+++ components/friends.js
@@ -0,0 +1,57 @@
+
+
+import React,{useEffect,useState} from 'react';
+import Head from 'next/head';
+import {Container,Row,Col} from 'reactstrap';
+import Scrollbar from  'react-scrollbars-custom';
+
+
+function Friends({friends}){
+    const take = (name)=>{
+        var res3
+        var divide = name.split(" ",2)
+        if(divide.length===1){
+            var res1 = divide[0].charAt(0)
+            res3 = res1
+        }else{
+            var res1 = divide[0].charAt(0);
+            var res2 = divide[1].charAt(0);
+            res3 = res1.concat(" "+res2)
+        }
+        //var res1 = divide[0].charAt(0);
+        //var res2 = divide[1].charAt(0);
+        //var res3 = res1.concat(" "+res2)
+        return res3
+    }
+
+    return(
+     
+        
+           <div >
+             
+                
+                
+                 <h1 className = "friend-text">Friends</h1>     
+                
+                <Scrollbar  noScrollX = {true} 
+                style = {{width:'100%',height:350,backgroundColor:"whitesmoke",marginTop:-10,}} 
+                thumbMinSize={30} className = "content-friends-data">
+                 
+                {friends.map((friend,i)=>friend.map((every)=>
+                <div>
+                <div className="friend-profile"><p className="each-name">{take(every.name)}</p></div>  
+                <p className = "friend-friends">{every.name}</p>
+                <p className="online-color">online</p>
+                </div>))}
+                 </Scrollbar>
+                
+                
+                 
+            </div>
+
+    )
+}
+
+
+
+export default Friends;
--- pages/index.js
+++ pages/index.js
@@ -1,203 +1,47 @@
-import Head from 'next/head'
 
-const Home = () => (
-  <div className="container">
-    <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+import React from 'react';
+//import logo from '../assets/images/video5.mp4';
+import logo from '../video7.mp4';
+import Head from 'next/head';
+import Box from '@material-ui/core/Box';
+function Facial(){
 
-    <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-      </h1>
+    return(
+        <div >
+       <Head>
+           <title>Letdis</title>
+           <meta charSet="utf-8" />
+           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+           <meta name="theme-color" content="#000000" />
+       </Head>
+       <Box display= "flex" flexDirection="row-reverse" style={{position:"absolute",zIndex:10}}>
+       <div className="top-button">
+           <span><a href = "http://localhost:3000/login">
+            <button className="facial-buttons">login</button>
+            </a></span>
+            <span><a href = "http://localhost:3000/register">
+            <button className="facial-buttons">Register</button>
+            </a></span>
+          
+       </div>
+       </Box>
+       <div className="facial-text">
+           <p className = "chat-logo">letdis</p>
+           <div className="inner-facial-text">
+           <p className="facial-text-p">Join conversations with friends  and get exposure to the things you like</p>
+          </div>
+          </div>
+         
+        <div style={{overFlow:"hidden",height:625}} className ="back-facial">
+       <video autoPlay loop muted 
+       style={{paddingRight:0,objectFit: 'cover',opacity:0.8,width:"100%",height:625,marginBottom:-60 }}>
+            <source src={logo} type = "video/mp4" />
+          
+       </video>
+         </div>
+       
+         </div>
+    )
+}
 
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
-      </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/new?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card"
-        >
-          <h3>Deploy &rarr;</h3>
-          <p>
-            Instantly deploy your Next.js site to a public URL with ZEIT Now.
-          </p>
-        </a>
-      </div>
-    </main>
-
-    <footer>
-      <a
-        href="https://zeit.co?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Powered by <img src="/zeit.svg" alt="ZEIT Logo" />
-      </a>
-    </footer>
-
-    <style jsx>{`
-      .container {
-        min-height: 100vh;
-        padding: 0 0.5rem;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      main {
-        padding: 5rem 0;
-        flex: 1;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer {
-        width: 100%;
-        height: 100px;
-        border-top: 1px solid #eaeaea;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      footer img {
-        margin-left: 0.5rem;
-      }
-
-      footer a {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-
-      a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
-        color: #0070f3;
-        text-decoration: none;
-      }
-
-      .title a:hover,
-      .title a:focus,
-      .title a:active {
-        text-decoration: underline;
-      }
-
-      .title {
-        margin: 0;
-        line-height: 1.15;
-        font-size: 4rem;
-      }
-
-      .title,
-      .description {
-        text-align: center;
-      }
-
-      .description {
-        line-height: 1.5;
-        font-size: 1.5rem;
-      }
-
-      code {
-        background: #fafafa;
-        border-radius: 5px;
-        padding: 0.75rem;
-        font-size: 1.1rem;
-        font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-          DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-      }
-
-      .grid {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        flex-wrap: wrap;
-
-        max-width: 800px;
-        margin-top: 3rem;
-      }
-
-      .card {
-        margin: 1rem;
-        flex-basis: 45%;
-        padding: 1.5rem;
-        text-align: left;
-        color: inherit;
-        text-decoration: none;
-        border: 1px solid #eaeaea;
-        border-radius: 10px;
-        transition: color 0.15s ease, border-color 0.15s ease;
-      }
-
-      .card:hover,
-      .card:focus,
-      .card:active {
-        color: #0070f3;
-        border-color: #0070f3;
-      }
-
-      .card h3 {
-        margin: 0 0 1rem 0;
-        font-size: 1.5rem;
-      }
-
-      .card p {
-        margin: 0;
-        font-size: 1.25rem;
-        line-height: 1.5;
-      }
-
-      @media (max-width: 600px) {
-        .grid {
-          width: 100%;
-          flex-direction: column;
-        }
-      }
-    `}</style>
-
-    <style jsx global>{`
-      html,
-      body {
-        padding: 0;
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-          Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-    `}</style>
-  </div>
-)
-
-export default Home
+export default Facial;
